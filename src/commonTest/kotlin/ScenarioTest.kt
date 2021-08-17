2dc08c2e7ab2a41e686fbc6111eda9341c0e761d
Carson Holzheimer <carson@redeye.co>
2021-08-17 23:27:04 +1000

    Rearrange test display

--- src/commonTest/kotlin/ScenarioTest.kt
+++ src/commonTest/kotlin/ScenarioTest.kt
@@ -4,7 +4,7 @@ import kotlin.time.Duration
 
 class ScenarioTest {
     val citizenContext = CitizenContext(
-        age = 30,
+        age = 35,
         gender = Gender.MALE,
         vaccinationA = VaccineFirstDoseEvent(
             vaccine = AstraZeneca,
@@ -36,20 +36,20 @@ class ScenarioTest {
         println("How many people in 100,000 have these outcomes")
         println("----------------------------------------------")
         println("-- No vaccine ever: --")
-        println("  Covid causing hospitalization      :" + noVaccineLifetimeRisk.hospitalization * 100_000.0)
         println("  Covid causing death                :" + noVaccineLifetimeRisk.mortality * 100_000.0)
+        println("  Covid causing hospitalization      :" + noVaccineLifetimeRisk.hospitalization * 100_000.0)
 
         println("-- AstraZeneca now: --")
-        println("  Side effect causing hospitalisation: " + outcome.vaccineAOutcome.sideEffectRisk.hospitalization * 100_000.0)
+        println("  Covid causing death                : " + outcome.vaccineAOutcome.residualCovidRisk.mortality * 100_000.0)
         println("  Side effect causing death          : " + outcome.vaccineAOutcome.sideEffectRisk.mortality * 100_000.0)
         println("  Covid causing hospitalization      : " + outcome.vaccineAOutcome.residualCovidRisk.mortality * 100_000.0)
-        println("  Covid causing death                : " + outcome.vaccineAOutcome.residualCovidRisk.mortality * 100_000.0)
+        println("  Side effect causing hospitalisation: " + outcome.vaccineAOutcome.sideEffectRisk.hospitalization * 100_000.0)
 
         println("-- Pfizer later: --")
-        println("  Side effect causing hospitalisation: " + outcome.vaccineBOutcome.sideEffectRisk.hospitalization * 100_000.0)
+        println("  Covid causing death                : " + outcome.vaccineBOutcome.residualCovidRisk.mortality * 100_000.0)
         println("  Side effect causing death          : " + outcome.vaccineBOutcome.sideEffectRisk.mortality * 100_000.0)
         println("  Covid causing hospitalization      : " + outcome.vaccineBOutcome.residualCovidRisk.mortality * 100_000.0)
-        println("  Covid causing death                : " + outcome.vaccineBOutcome.residualCovidRisk.mortality * 100_000.0)
+        println("  Side effect causing hospitalisation: " + outcome.vaccineBOutcome.sideEffectRisk.hospitalization * 100_000.0)
     }
 
 }
